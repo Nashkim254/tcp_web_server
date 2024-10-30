8469ac8f5b1104609ec194dc3cc4cab96217929b
programming6131 <kimutainashon@gmail.com>
2024-10-30 12:16:37 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,19 @@ fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("index.html").unwrap();
-
-        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {}\r\n\r\n{}",
-                               contents.len(),
-                               contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    // let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        (" HTTP/1.1 200 OK", "index.html")
     } else {
-        let status_line = " HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        );
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+        (" HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
